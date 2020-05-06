1ce977930cf148c5b8d8428b101fc59abbc2f7dd
alan1811 <si16.alanruslan@mhs.ubpkarawang.ac.id>
2020-05-07 06:51:48 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,16 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi rest api ku berjalan", res)
 };
+
+//menampilkan semua data mahasiswa
+
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * from mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            console.error();
+        }
+        else {
+            response.ok(rows, res)
+        }
+    });
+};
